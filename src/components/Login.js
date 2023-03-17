9af517814ede1086b5714c75d308e70c0d47b3f2
Ruhua Wang <ruhuawan@usc.edu>
2023-03-16 17:17:29 -0700

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, message } from 'antd';
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+import { BASE_URL } from "../constants";
+
+function Login(props) {
+    const { handleLoggedIn } = props;
+
+    const onFinish = (values) => {
+        const { username, password } = values;
+        const opt = {
+            method: "POST",
+            url: `${BASE_URL}/signin`,
+            data: {
+                username: username,
+                password: password
+            },
+            headers: { "Content-Type": "application/json" }
+        };
+        axios(opt)
+            .then((res) => {
+                if (res.status === 200) {
+                    const { data } = res;
+                    handleLoggedIn(data);
+                    message.success("Login succeed! ");
+                }
+            })
+            .catch((err) => {
+                console.log("login failed: ", err.message);
+                message.error("Login failed!");
+            });
+
+    };
+
+    return (
+        <Form className="login-form"
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+        >
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <Link to="/register">register now!</Link>
+            </Form.Item>
+        </Form>
+    );
+}
+
+export default Login;
